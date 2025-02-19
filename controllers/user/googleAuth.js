4b863a9c773851b251dc85ed7d69998b0d650044
NISHANT SHARMA <nishant735123@gmail.com>
2025-02-19 18:23:21 +0530

    cors error

--- controllers/user/googleAuth.js
+++ controllers/user/googleAuth.js
@@ -37,10 +37,11 @@ const googleAuth = async (req, res) => {
         // 🍪 Store Token in HTTP-only Cookie
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "Strict",
+            secure: true,  
+            sameSite: "None",  // Required for cross-origin cookies
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
+
 
         res.json({ success: true, message: "Login successful", user });
 
