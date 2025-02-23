8ce3f3286b00700acfb9c66f4b0d5db99e5ff47f
NISHANT SHARMA <nishant735123@gmail.com>
2025-02-23 18:29:42 +0530

    cookie options

--- controllers/user/googleAuth.js
+++ controllers/user/googleAuth.js
@@ -34,12 +34,12 @@ const googleAuth = async (req, res) => {
             expiresIn: "7d",
         });
 
-        // 🍪 Store Token in HTTP-only Cookie
+       
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,  
-            sameSite: "strict",  // Required for cross-origin cookies
-            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+            sameSite: "none", 
+            maxAge: 7 * 24 * 60 * 60 * 1000, 
         });
 
 
