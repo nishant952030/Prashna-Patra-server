8a7474999041abebdf606de3117bffa030c33108
NISHANT SHARMA <nishant735123@gmail.com>
2025-02-23 18:36:02 +0530

    again the cringe thing

--- controllers/user/googleAuth.js
+++ controllers/user/googleAuth.js
@@ -34,12 +34,11 @@ const googleAuth = async (req, res) => {
             expiresIn: "7d",
         });
 
-       
         res.cookie("token", token, {
             httpOnly: true,
-            secure: true,  
-            sameSite: "none", 
-            maxAge: 7 * 24 * 60 * 60 * 1000, 
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
 
